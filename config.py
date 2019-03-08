fa9281011476504412a3dd43d667cf7ac7866d89
Yassine ABOUKIR <yaabouki.r@gmail.com>
2019-03-08 13:17:41 +0530

    adding config file

---
+++ config.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+# Slack webhooks for notifications
+posting_webhook = "https://hooks.slack.com/services/<secret>"
+errorlogging_webhook = "https://hooks.slack.com/services/<secret>"
+
+# crtsh postgres credentials
+DB_HOST = 'crt.sh'
+DB_NAME = 'certwatch'
+DB_USER = 'guest'
+DB_PASSWORD = ''
