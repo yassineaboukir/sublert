dc208fb167506482a30cb27da89c990ae3e1d9ca
Yassine ABOUKIR <yaabouki.r@gmail.com>
2019-03-13 02:54:59 +0700

    properly check for already monited domains

--- sublert.py
+++ sublert.py
@@ -229,7 +229,7 @@ def adding_new_domain(q1): #adds a new domain to the monitoring list
         else: pass
         with open("domains.txt", "r+") as domains: #checking domain name isn't already monitored
             for line in domains:
-                if domain_to_monitor in line:
+                if domain_to_monitor == line:
                     print(colored("[!] The domain name {} is already being monitored.".format(domain_to_monitor), "red"))
                     sys.exit(1)
             response = cert_database().lookup(domain_to_monitor)
