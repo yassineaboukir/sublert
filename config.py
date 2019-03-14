bb5da652f73f2b151e04e5de54b2d9003cd4e6da
Ross Simpson <ross.simpson@envato.com>
2019-03-14 12:26:53 +1100

    Make Slack sleep configurable
    Move the Slack free/pro sleep setting into configuration so users don't
    have to edit to the code to change it.

--- config.py
+++ config.py
@@ -3,6 +3,7 @@
 # Slack webhooks for notifications
 posting_webhook = "https://hooks.slack.com/services/<secret>"
 errorlogging_webhook = "https://hooks.slack.com/services/<secret>"
+slack_sleep_enabled = True  # bypass Slack rate limit when using free workplace, remove this line if you've pro subscription
 
 # crtsh postgres credentials
 DB_HOST = 'crt.sh'
--- sublert.py
+++ sublert.py
@@ -107,7 +107,8 @@ def slack(data): #posting to Slack
     if response.status_code != 200:
         error = "Request to slack returned an error {}, the response is:\n{}".format(response.status_code, response.text)
         errorlog(errorlog, enable_logging)
-    time.sleep(1) #bypass Slack rate limit when using free workpalce, remove this line if you've pro subscription
+    if slack_sleep_enabled:
+        time.sleep(1)
 
 def reset(do_reset):
     if do_reset:
