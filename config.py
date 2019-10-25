0fcd825e599627e4452611e84d7d936f494e1046
Yassine ABOUKIR <yassine@aboukir.me>
2019-10-26 00:12:09 +0700

    remove slack error posting webhook

--- config.py
+++ config.py
@@ -2,7 +2,6 @@
 
 # Slack webhooks for notifications
 posting_webhook = "https://hooks.slack.com/services/<secret>"
-errorlogging_webhook = "https://hooks.slack.com/services/<secret>"
 slack_sleep_enabled = True  # bypass Slack rate limit when using free workplace, switch to False if you're using Pro/Ent version.
 at_channel_enabled = True   # Add @channel notifications to Slack messages, switch to False if you don't want to use @channel
 
